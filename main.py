11a3e5624d09457af77a9e494b1936da4be8f8ca
Abstras-Dev <lenzo24733@gmail.com>
2025-07-29 06:08:24 -0400

    Cambio realizado en el main: cambio realizado en el uvicorn.run(), eliminando el reload

--- main.py
+++ main.py
@@ -44,4 +44,4 @@ async def health_check():
     return {"status": "healthy"}
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=int(os.environ.get("PORT", 8000)), reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
